f341de0386b4b1e366f2d24186a444dc7ee7541c
isamu-isozaki <isamu.website@gmail.com>
2023-06-30 19:12:32 -0400

    Fix code losses

--- examples/controlnet/train_controlnet_qr.py
+++ examples/controlnet/train_controlnet_qr.py
@@ -1111,17 +1111,17 @@ def main(args):
                 original_pred_latent, _ = get_original(noise_scheduler, model_pred, noisy_latents, timesteps)
                 decoded_original_pred = vae.decode(original_pred_latent).sample/vae.config.scaling_factor
                 decoded_original_pred = (decoded_original_pred+1)/2.
-                code_losses = []
+                code_losses = 0
                 for i in range(bsz):
                     decoded_original_pred[i] = qrcode_transform(decoded_original_pred[i])
                     controlnet_image[i] = qrcode_transform(controlnet_image[i])
                     predicted_qr = F.interpolate(decoded_original_pred[i][None], size=(qrcode_sizes[i], qrcode_sizes[i]), mode="bicubic")
                     code_target = F.interpolate(controlnet_image[i][None], size=(qrcode_sizes[i], qrcode_sizes[i]), mode="bicubic") > 0.5
                     if args.qr_bce_loss:
-                        code_losses.append(F.binary_cross_entropy(torch.clip(torch.flatten(predicted_qr.float()), min=args.label_smoothing, max=1-args.label_smoothing) , torch.flatten(code_target.float()), reduction="mean"))
+                        code_losses += F.binary_cross_entropy(torch.clip(torch.flatten(predicted_qr.float()), min=args.label_smoothing, max=1-args.label_smoothing) , torch.flatten(code_target.float()), reduction="mean")
                     else:
-                        code_losses.append(F.mse_loss(predicted_qr.float() > 0.5, code_target.float(), reduction="mean"))
-                code_loss = torch.mean(code_losses)
+                        code_losses += F.mse_loss(predicted_qr.float() > 0.5, code_target.float(), reduction="mean")
+                code_loss = code_losses / bsz
                 content_loss = F.mse_loss(model_pred.float(), target.float(), reduction="mean")
                 loss = content_loss + args.code_weight* code_loss
 
